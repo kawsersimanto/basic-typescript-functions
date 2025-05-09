45f96172d90e38ad0c5733821caebf54addb423f
kawserimanto <kawsersimanto@gmail.com>
2025-05-09 11:30:45 +0600

    added formatString function

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,12 @@
 "use strict";
+{
+    function formatString(input, toUpper = true) {
+        if (toUpper) {
+            return input.toUpperCase();
+        }
+        return input.toLocaleLowerCase();
+    }
+    console.log(formatString("Hello"));
+    console.log(formatString("Hello", true));
+    console.log(formatString("Hello", false));
+}
