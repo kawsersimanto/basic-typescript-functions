2468bc0906531fc3993795c97523fd753d8e5194
Kawser Simanto <kawsersimanto@gmail.com>
2025-05-09 12:16:59 +0600

    created filter by rating function

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,16 @@
   function formatString(input: string, toUpper: boolean = true): string {
     return toUpper ? input.toUpperCase() : input.toLowerCase();
   }
+
+  function filterByRating(
+    items: { title: string; rating: number }[]
+  ): { title: string; rating: number }[] {
+    return items?.filter((item) => item?.rating >= 4 && item?.rating <= 5);
+  }
+
+  const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 },
+  ];
 }
